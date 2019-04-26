6a5bca3ce8b732403014434f69d3e5ede80310ab
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-26 16:20:07 +0700

    Add files via upload

---
+++ toolsv3.sh
@@ -0,0 +1,199 @@
+#!/system/xbin/bash
+#mau ngapain cok ?
+#mau ngerecode kwkwkw kocak cok ?
+#pake otak dong kongtoll (_)(_)=========D
+#tu recod aja gabakal bisa jadi :v wkkwkw
+clear
+figlet STARLA | lolcat
+echo "■■■■■■■loading 25%" | lolcat
+echo "■■■■■■■■■■■■■■■■■loading 50%" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■loading 75%" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■100%" | lolcat
+echo "SUKSES DOWNLOAD"
+echo
+echo "[][][][][[][][][][][][][][][][][][][][][][]" | lolcat
+echo
+echo "AUTHOR BIGBOS STARLA" | lolcat
+echo "FRIENDS : ALL CYBER"
+echo "CYB3R POP1CE +62" | lolcat
+echo "HAPPY HACKING TEAM"
+echo "PLISS SUBSCRIBE CHANEL SAYA" | lolcat
+echo "chanel saya namanya : Bigbos Starla"
+echo "PLISS SUBSCRIBE YAKK" | lolcat
+echo
+echo "[][][][][][][][][][][][][][][][][][][][][][]" | lolcat
+sleep 2
+
+clear
+figlet LOGIN DULU | lolcat
+echo
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "Masukan Kode Untuk Masuk ke Tools install"
+echo "WEBSCAN"
+echo
+echo " 8761.  9176.  0000.  8761.  1927.   4142. " | lolcat
+echo
+echo " 8713.  8888.  5172.  7654.  1235.   8762. " | lolcat
+echo
+echo "MASUKAN KODENYA KALAU GAK TAU PM SAYA SAJA"
+echo "FB GUE Bigbos Starla Sekalian Ikuti yahh.." | lolcat
+echo
+echo
+read -p "☆B16b05St4r14☆##" bro
+
+if [ $bro = 8761 ]
+then
+clear
+figlet Gagal | lolcat
+echo
+echo "PM GUE KALAU GAK TAU CUK!!" | lolcat
+echo "TUNGGU 5 DETIK YAH BROO!!" | lolcat
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 9176 ]
+then
+clear
+figlet Gagal
+echo
+echo "PM GUE KALAU GAK TAU CUK!!" | lolcat
+echo "TUNGGU 5 DETIK YAH BROO!!"
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 0000 ]
+then
+clear
+figlet Gagal | lolcat
+echo
+echo "PM GUE KALAU GAK TAU CUK!!"
+echo "TUNGGU 5 DETIK YAH BROO!!" | lolcat
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 8761 ]
+then
+clear
+figlet Gagal | lolcat
+echo
+echo "PM GUE KALAU GAK TAU CUK!!"
+echo "TUNGGU 5 DETIK YAH BROO!!" | lolcat
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 1927 ]
+then
+clear
+figlet Gagal | lolcat
+echo
+echo "PM GUE KALAU GAK TAU CUK!!"
+echo "TUNGGU 5 DETIK YAH BROO!!" | lolcat
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 4142 ]
+then
+clear
+figlet BERHASIL | lolcat
+echo
+echo "LOGIN BERHASIL" | lolcat
+echo "LANGSUNG KE TOOLSNYA YAH"
+sleep 1
+echo "99% INSTALL WEBSCAN" | lolcat
+sleep 2
+apt-get update && apt-get upgrade
+apt install ruby
+apt install curl
+apt install git
+git clone https://github.com/wpscanteam/wpscan
+cd ~/wpscan
+gem install bundle
+bundle config build.nokogiri --use-system-libraries
+bundle install
+ruby wpscan.rb --update
+cd ~/
+echo " wpscannya udah di install tu bree " | lolcat
+echo "tinggal dipake jangan lupa pake kondom :v" | lolcat
+fi
+
+if [ $bro = 8713 ]
+then
+clear
+figlet Gagal | lolcat
+echo
+echo "PM GUE KALAU GAK TAU CUK!!"
+echo "TUNGGU 5 DETIK YAH BROO!!" | lolcat
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 8888 ]
+then
+clear
+figlet Gagal
+echo
+echo "PM GUE KALAU GAK TAU CUK!!" | lolcat
+echo "TUNGGU 5 DETIK YAH BROO!!"
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 5172 ]
+then
+clear
+figlet Gagal | lolcat
+echo
+echo "PM GUE KALAU GAK TAU CUK!!"
+echo "TUNGGU 5 DETIK YAH BROO!!" | lolcat
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 7654 ]
+then
+clear
+figlet Gagal | lolcat
+echo
+echo "PM GUE KALAU GAK TAU CUK!!"
+echo "TUNGGU 5 DETIK YAH BROO!!" | lolcat
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 1235 ]
+then
+clear
+figlet Gagal
+echo
+echo "PM GUE KALAU GAK TAU CUK!!" | lolcat
+echo "TUNGGU 5 DETIK YAH BROO!!"
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
+
+if [ $bro = 8762 ]
+then
+clear
+figlet Gagal
+echo
+echo "PM GUE KALAU GAK TAU CUK!!" | lolcat
+echo "TUNGGU 5 DETIK YAH BROO!!"
+sleep 4
+cd toolsv3
+sh toolsv3.sh
+fi
